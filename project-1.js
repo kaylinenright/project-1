a1d986c659f409d2fcaf824742744fdc13e38077
Kay-babycakes <kaylin.enright@hotmail.com>
2015-03-16 18:36:09 -0400

    added transitions and images

--- project-1.js
+++ project-1.js
@@ -1,7 +1,12 @@
 var $burger = $('.burger');
 var $panel = $('.panel');
  var $btnShowHide = $(".btn-show-hide");
+var $btn3 = $(".btn-3");
+var $btn2 = $(".btn-2");
 var $portfolio = $(".portfolio");
+var $portfolio2 = $(".portfolio-2");
+var $portfolio3 = $(".portfolio-3");
+
 
 
 $burger.on ('click', function () {
@@ -11,4 +16,12 @@ $burger.on ('click', function () {
 
 $btnShowHide.on("click", function () {
     $portfolio.toggleClass("js-box-show-hide");
+});
+
+$btn2.on("click", function () {
+    $portfolio2.toggleClass("js-box-show-hide");
+});
+
+$btn3.on("click", function () {
+    $portfolio3.toggleClass("js-box-show-hide");
 });
