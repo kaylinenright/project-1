0e226feef49186cbfbc7d448782ba8df55b9f332
Kay-babycakes <kaylin.enright@hotmail.com>
2015-03-16 17:08:12 -0400

    added grid

--- project-1.js
+++ project-1.js
@@ -1,6 +1,14 @@
 var $burger = $('.burger');
 var $panel = $('.panel');
+ var $btnShowHide = $(".btn-show-hide");
+var $portfolio = $(".portfolio");
+
 
 $burger.on ('click', function () {
     $panel.toggleClass('js-panel-expand');
+});
+
+
+$btnShowHide.on("click", function () {
+    $portfolio.toggleClass("js-box-show-hide");
 });
